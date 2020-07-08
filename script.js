c26d6a9fdfc665cfa4066f8cfb98084ae8d4f148
Leonardo Vimieiro <leonardo.vimieiro@gmail.com>
2020-07-07 22:48:11 -0300

    definindo as direcoes

--- script.js
+++ script.js
@@ -20,7 +20,23 @@ function criarCobrinha(){
     } 
 }
 
+document.addEventListener("keydown", update);
+
+
+function update(event){  //verifica qual tecla foi apertada e nao pode ser a tecla contraria à direçao que a cobra ja esta andando
+    console.log(event.keyCode);
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG(); 
     criarCobrinha(); 
 
